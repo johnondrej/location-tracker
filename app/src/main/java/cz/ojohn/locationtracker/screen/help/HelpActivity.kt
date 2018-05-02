0d199a5bafeb990f696aa02635ef4682c048b659
Ondřej John <johnondr@fit.cvut.cz>
2018-05-02 14:52:14 +0200

    Display up arrow in Settings and Help activities

--- app/src/main/java/cz/ojohn/locationtracker/screen/help/HelpActivity.kt
+++ app/src/main/java/cz/ojohn/locationtracker/screen/help/HelpActivity.kt
@@ -2,6 +2,7 @@ package cz.ojohn.locationtracker.screen.help
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import cz.ojohn.locationtracker.R
 import cz.ojohn.locationtracker.screen.main.MainActivity
 
@@ -13,10 +14,20 @@ class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .add(R.id.helpFragmentContainer, HelpFragment.newInstance(intent.extras.getInt(MainActivity.SCREEN_KEY)))
                     .commit()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/cz/ojohn/locationtracker/screen/settings/SettingsActivity.kt
+++ app/src/main/java/cz/ojohn/locationtracker/screen/settings/SettingsActivity.kt
@@ -2,6 +2,7 @@ package cz.ojohn.locationtracker.screen.settings
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import cz.ojohn.locationtracker.R
 
 /**
@@ -12,11 +13,20 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .add(R.id.fragmentContainer, SettingsFragment())
                     .commit()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
